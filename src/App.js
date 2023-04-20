14002b2a5179cb7f7ee6a5b62a88d7e4e5d11105
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-04-20 23:39:39 +0300

    game over

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   // hover:scale-110 hover:bg-indigo-700 hover:rounded-lg hover:text-pink-300
 
   // [...Array(9)]
-  const tickTackToe =  [...Array(9)]
-  
-  const [values, setValues] = useState(tickTackToe)
+  const ticTacToe =  [...Array(9)]
+  const [values, setValues] = useState(ticTacToe)
   const [turn, setTurn] = useState(true)
+  const [isGameOver, setIsGameOver] = useState(false)
 
   const winnerPoints = [
     [0, 1, 2],
@@ -22,26 +22,48 @@ function App() {
   ]
 
   const handleClick = ({target}, index) => {
+    values[index] = turn ? 'X' : 'O'
 
-    if(turn) {
-      values[index] = 'X'
-    }else {
-      values[index] = 'O'
-    }
-    
     setTurn(!turn)
     target.innerText = values[index]
     setValues(values)
+    isGameFinished(values[index])
+  }
+
+  const isGameFinished = (sign) => {
+    const points = values
+        .map((val, index) => val === sign ? index : -1)
+        .filter(val => val !== -1)
+
+    for (const winnerPoint of winnerPoints) {
+      const intersection = winnerPoint.filter(val => points.includes(val))
+
+      if (intersection.length === 3) {
+        alert(`${sign} OYUNU KAZANDI`)
+        setIsGameOver(true)
+      }
+    }
+
+    console.log({
+      SIGN: sign,
+      values, points
+    })
+  }
+
+  const playAgain = () => {
+    setIsGameOver(false)
+    setTurn(true)
+    setValues(ticTacToe)
   }
 
   return (
     <div className="container">
-      {/* <h1 className="turn-header">{turn ? 'X': 'O'}</h1> */}
+      {isGameOver && <button className="btn" onClick={playAgain}>Play Again</button>}
       <div className="game-board">
         {values.map((value, index)=>(
-          <span className={`board-piece ${index%2 === 0 ? 'hakan' : null }`} key={index} onClick={(e) => {handleClick(e,index)}} sign={turn ? 'X': 'O'}>{value}</span>
+          <span className={`board-piece ${typeof values[index] === 'string' || isGameOver ? 'hakan' : null }`} key={index} onClick={(e) => {handleClick(e,index)}} sign={turn ? 'X': 'O'}>{value}</span>
         ))}
-        <input type="checkbox" name="" id="deneme" />
+        {/*<input type="checkbox" name="" id="deneme" />*/}
         <div className="line"></div>
       </div>
     </div>
